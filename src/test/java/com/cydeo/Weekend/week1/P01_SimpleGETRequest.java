5157e4a36d8db38e700fd37c019c48b01ca18bd3
sunisaservais <sunisaservais@gmaill.com>
2025-07-06 16:09:46 -0400

    file added

--- src/test/java/com/cydeo/Weekend/week1/P01_SimpleGETRequest.java
+++ src/test/java/com/cydeo/Weekend/week1/P01_SimpleGETRequest.java
@@ -13,22 +13,16 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class P01_SimpleGETRequest {
 
-    /**
-     * Requirements:
-     * - Given accept type is Json
-     * - And base URI: http://cydeo.onthewifi.com:1000/ords/hr
-     * - When user sends GET request to endpoint:/regions
-     * - Store the response in Response Object that comes from GET Request
-     * - Print out followings:
-     * - Headers
-     * - Content-Type
-     * - Status Code
-     * - Response
-     * - Date
-     * - Then verify followings:
-     * - Response body has "Europe"
-     * - Response headers has "Date"
-     */
+
+    //Gherkin Language:
+    //Given--> condition that you have currently
+    //When --> action that you will do
+    //Then --> verification that you will verify
+
+    //     * - Given accept type is Json
+    //     * - And base URI: http://cydeo.onthewifi.com:1000/ords/hr
+    //     * - When user sends GET request to endpoint:/regions
+    //     * - Store the response in Response Object that comes from GET Request
 
     @DisplayName("Get Request")
     @Test
@@ -41,6 +35,13 @@ public class P01_SimpleGETRequest {
                 .baseUri(baseURI)
                 .when()
                 .get("/regions");
+
+        //     * - Print out followings:
+        //     - Headers
+        //     - Content-Type
+        //     - Status Code
+        //     - Response
+        //     - Date
 
         System.out.println("-------------------HEADERS-------------------");
         System.out.println("response.headers() = " + response.headers());
