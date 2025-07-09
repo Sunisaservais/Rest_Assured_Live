47b4b87036343355a4b85bd1f862ceb4c413a336
sunisaservais <sunisaservais@gmaill.com>
2025-07-09 16:21:53 -0400

    updates

--- src/test/java/com/cydeo/liveLabs/Homework1.java
+++ src/test/java/com/cydeo/liveLabs/Homework1.java
@@ -2,13 +2,16 @@ package com.cydeo.liveLabs;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.jupiter.api.Assertions.*;
 
-public class P01 {
+public class Homework1 {
+
+    //Document: https://documenter.getpostman.com/view/13495747/2sAYdkFoc6
 
     /**
      * Task 1 :
@@ -20,7 +23,7 @@ public class P01 {
      * - And response contains United States of America
      */
 
-    @DisplayName("GET United States of America")
+    @DisplayName("Request United States of America")
     @Test
     public void test1() {
         baseURI = "http://cydeo.onthewifi.com:1000/ords/hr";
@@ -33,12 +36,13 @@ public class P01 {
         //response.prettyPrint();
 
         //Verify status code is 200
-        assertEquals(200, response.getStatusCode());
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode());
 
         //Verify Content - Type is application/json
         assertEquals(ContentType.JSON.toString(), response.getContentType());
 
         //Verify response contains United States of America
+        //assertEquals("United States of America", response.path("country_name"));
         assertTrue(response.getBody().asString().contains("United States of America"));
     }
 
@@ -63,7 +67,7 @@ public class P01 {
         //response.prettyPrint();
 
         //Verify status code is 404
-        assertEquals(404, response.getStatusCode());
+        assertEquals(HttpStatus.SC_NOT_FOUND, response.getStatusCode());
     }
 
     /**
@@ -78,7 +82,7 @@ public class P01 {
      * - And "Transfer-Encoding" should be "chunked"
      */
 
-    @DisplayName("GET Europe")
+    @DisplayName("Request Europe")
     @Test
     public void test3() {
         baseURI = "http://cydeo.onthewifi.com:1000/ords/hr";
@@ -86,21 +90,22 @@ public class P01 {
                 .accept(ContentType.JSON)
                 .and()
                 .when()
-                .get("/regions/10");
+                .get(baseURI+"/regions/10");
 
         //response.prettyPrint();
 
         //Verify status code is 200
-        assertEquals(200, response.getStatusCode());
+        assertEquals(HttpStatus.SC_OK, response.getStatusCode());
 
         //Verify Content - Type is application/json
         assertEquals(ContentType.JSON.toString(), response.getContentType());
 
         //Verify response contains Europe
-        assertTrue(response.getBody().asString().contains("Europe"));
+        assertTrue(response.asString().contains("Europe"));
 
         //Verify header should contains Date
         assertTrue(response.getHeaders().hasHeaderWithName("Date"));
+        assertTrue(response.getHeaders().hasHeaderWithName("date"));
 
         //Verify "Transfer-Encoding" should be "chunked"
         assertEquals("chunked", response.getHeader("Transfer-Encoding"));
